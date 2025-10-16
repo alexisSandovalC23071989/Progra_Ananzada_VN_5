29cdbcd3b32e4badf23ac276bcc21081f1afcdf8
Alexis <aldavid23@gmail.com>
2025-10-15 22:59:25 -0600

    DTO

--- Proyecto_Programacion/obj/Debug/net8.0/Proyecto_Programacion.AssemblyInfo.cs
+++ Proyecto_Programacion/obj/Debug/net8.0/Proyecto_Programacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Programacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556b0dd0d72c9f9cb5f9b5818216eedc7251b432")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Programacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Programacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
